a607b3a88b3e55ffd94dbe67d64cab25bde3d460
Cauê Shimoda <cauefshimoda@gmail.com>
2025-09-12 18:53:34 -0300

    Update Animal.java

--- src/main/java/com/miaudote/controller/AnimalController.java
+++ src/main/java/com/miaudote/controller/AnimalController.java
@@ -1,5 +1,7 @@
 package com.miaudote.controller;
 
+import com.miaudote.model.Animal;
+
 public class AnimalController {
 
 }
--- src/main/java/com/miaudote/model/Animal.java
+++ src/main/java/com/miaudote/model/Animal.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 public class Animal {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @ManyToOne
