224ef5ed93033165f999ed3378f59c3290bdae75
Samuel Leite Pereira <programmer.and.investor@gmail.com>
2025-10-23 10:17:29 -0300

    Fix do CORS para permitir acesso de qualquer servidor

--- src/main/java/com/miaudote/config/CorsConfig.java
+++ src/main/java/com/miaudote/config/CorsConfig.java
@@ -14,8 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        // .allowedOrigins("http://localhost:3000") // ex: seu front em React
-                        .allowedOrigins("*") // ex: seu front em React
+                        .allowedOrigins("http://localhost:3000", "https://http-client-weld.vercel.app") // ex: seu front em React
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
